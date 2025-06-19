ae4f0bd0fe864f1624aa6ec84a0893248df93013
ngliern <logonbbjn@gmail.com>
2025-06-19 16:48:47 +0800

    Update Adminhome.jsx

--- client/src/pages/admin/Adminhome.jsx
+++ client/src/pages/admin/Adminhome.jsx
@@ -1,4 +1,4 @@
-function Home() {
+function AdminHome() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">Welcome to the Admin Home Page</h1>
@@ -6,4 +6,4 @@ function Home() {
     </div>
   );
 }
-export default Home;
+export default AdminHome;
