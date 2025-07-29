75d640ab56fc1fcc25973f3eaaa0b5ac909853d3
KristanLloydBDC <kristnthings@gmail.com>
2025-07-29 17:42:00 +0800

    Added Escalations table and gave client.js a new association.

--- server/models/client.js
+++ server/models/client.js
@@ -21,6 +21,7 @@ module.exports = (sequelize, DataTypes) => {
 
     Client.associate = (models) => {
         Client.hasMany(models.ClientMessage, { foreignKey: 'senderPhone', sourceKey: 'phoneNumber' });
+        Client.hasMany(models.Escalation, { foreignKey: 'clientId', sourceKey: 'id' })
     };
 
     return Client;
